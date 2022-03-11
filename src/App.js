a4e92c776ea57e276ef5c3389cdf4bdf5e8f1a47
pgull <preston@latitude.io>
2022-03-10 23:12:25 -0500

    init commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './liftcincy-wob-hero.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="lift-cincy-temp-hero-container">
+      <img src={logo} className="lift-cincy-landing-logo" alt="Lift Cincy Logo"/>
+    </div>
+  );
+}
+
+export default App;
